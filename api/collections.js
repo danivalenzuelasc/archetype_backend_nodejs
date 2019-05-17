3f6faca33eb8f00cdeef48ce6e8c63eabcfd60b8
Daniel Valenzuela <daniel.valenzuela@softcrum.com>
2019-05-17 07:19:40 -0400

    Initial project

--- api/collections.js
+++ api/collections.js
@@ -4,54 +4,6 @@ module.exports = () => {
     address: {
       import: false,
     },
-    city: {
-      import: false,
-    },
-    contact: {
-      import: false,
-    },
-    country: {
-      import: false,
-    },
-    currency: {
-      import: false,
-    },
-    email: {
-      import: false,
-    },
-    language: {
-      import: false,
-    },
-    mailing: {
-      import: false,
-    },
-    minister: {
-      import: false,
-    },
-    neighborhood: {
-      import: false,
-    },
-    parish: {
-      import: false,
-    },
-    permission: {
-      import: false,
-    },
-    phone: {
-      import: false,
-    },
-    social_network: {
-      import: false,
-    },
-    state: {
-      import: false,
-    },
-    timezone: {
-      import: false,
-    },
-    user: {
-      import: false,
-    },
   };
   Object.keys(api).forEach((collection) => {
     api[collection].mocks = require(`./${collection}/${collection}.mocks`);
