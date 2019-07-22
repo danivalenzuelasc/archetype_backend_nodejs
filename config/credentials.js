1b5a7fc93323328a0348ca960ec541dcf7f91fd2
Daniel Valenzuela <daniel.valenzuela@softcrum.com>
2019-07-22 16:32:27 -0400

    Add MongoDB Atlas in architecture

--- config/credentials.js
+++ config/credentials.js
@@ -1,5 +1,14 @@
 // Se exportan las variables de las credenciales
 module.exports = {
-  mongodb: process.env.MONGO_URI || 'mongodb://localhost/integraciones_backend_cl',
+  mongodb: process.env.MONGO_URI ||
+    process.env.NODE_ENV === 'production'
+    ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+    : process.env.NODE_ENV === 'certification'
+      ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+      : process.env.NODE_ENV === 'release'
+        ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+        : process.env.NODE_ENV === 'development'
+          ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+          : 'mongodb://localhost/integraciones_backend_cl',
   sentry: process.env.SENRTY_API_KEY || 'https://ceadbc8659fb4e9ba5eb1bb39873665d@sentry.io/1485189',
 };
