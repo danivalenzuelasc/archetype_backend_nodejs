07a435f8887b08db97ff41e289d655bbe546fad6
Daniel Valenzuela <daniel.valenzuela@softcrum.com>
2019-07-24 18:49:12 -0400

    Update credentials MongoDB Atlas

--- config/credentials.js
+++ config/credentials.js
@@ -1,14 +1,17 @@
+// Se declaran las dependencias
+const settings = require('./settings');
+
 // Se exportan las variables de las credenciales
 module.exports = {
   mongodb: process.env.MONGO_URI ||
     process.env.NODE_ENV === 'production'
-    ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+    ? `mongodb+srv://${settings.mongo.production.user}:${settings.mongo.production.password}@${settings.mongo.production.server}`
     : process.env.NODE_ENV === 'certification'
-      ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+      ? `mongodb+srv://${settings.mongo.certification.user}:${settings.mongo.certification.password}@${settings.mongo.certification.server}`
       : process.env.NODE_ENV === 'release'
-        ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+        ? `mongodb+srv://${settings.mongo.release.user}:${settings.mongo.release.password}@${settings.mongo.release.server}`
         : process.env.NODE_ENV === 'development'
-          ? 'mongodb+srv://root:WPm8nKdA6kkT11uP@nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority'
+          ? `mongodb+srv://${settings.mongo.development.user}:${settings.mongo.development.password}@${settings.mongo.development.server}`
           : 'mongodb://localhost/integraciones_backend_cl',
   sentry: process.env.SENRTY_API_KEY || 'https://ceadbc8659fb4e9ba5eb1bb39873665d@sentry.io/1485189',
 };
--- config/settings.js
+++ config/settings.js
@@ -6,6 +6,28 @@ module.exports = {
     hashing: process.env.HASHING || 43200000,
     limit: process.env.LIMIT || 500,
   },
+  mongo: {
+    certification: {
+      password: '',
+      server: '',
+      user: '',
+    },
+    development: {
+      password: 'WPm8nKdA6kkT11uP',
+      server: 'nubox-matrix-dev-ugnk2.mongodb.net/integraciones_backend_cl?retryWrites=true&w=majority',
+      user: 'root',
+    },
+    production: {
+      password: '',
+      server: '',
+      user: '',
+    },
+    release: {
+      password: '',
+      server: '',
+      user: '',
+    },
+  },
   port: process.env.PORT || 4000,
   windowsService: 'E:\\Nubox\\integraciones_backend_cl\\app.js',
 };
